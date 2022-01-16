72bdaca90b445692a76563f7da8f35c53bbedd8e
Rajeev Kumar Singh <callicoder@gmail.com>
2022-01-16 16:29:27 +0530

    brownie fund me test

--- 5-brownie/fund-me/tests/test_fund_me.py
+++ 5-brownie/fund-me/tests/test_fund_me.py
@@ -1,5 +1,7 @@
-from scripts.helpers import get_account
+from brownie import network, accounts, exceptions
+from scripts.helpers import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
+import pytest
 
 
 def test_can_fund_and_withdraw():
@@ -14,3 +16,12 @@ def test_can_fund_and_withdraw():
     tx2 = fund_me.withdraw({"from": account})
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
