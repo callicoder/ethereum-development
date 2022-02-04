876af7b171c120079d242c288528c40605f29cbd
Rajeev Kumar Singh <callicoder@gmail.com>
2022-02-04 19:41:18 +0530

    lottery finish

--- 5-brownie/smartcontract-lottery/contracts/Lottery.sol
+++ 5-brownie/smartcontract-lottery/contracts/Lottery.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
+    address payable public recentWinner;
     uint256 public usdEntryFee;
+    uint256 public randomness;
     AggregatorV3Interface internal ethUsdPriceFeed;
     enum LOTTERY_STATE {
         OPEN,
@@ -14,11 +17,22 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lotteryState;
+    bytes32 internal keyHash; // Way to uniquely identify the chainlink VRF node (public key against which randomness is generated)
+    uint256 internal fee; // fee required to fulfill a VRF request
+    event RequestedRandomness(bytes32 requestId);
 
-    constructor(address _priceFeed) {
+    constructor(
+        address _priceFeed,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyHash
+    ) VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * 10**18;
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeed);
         lotteryState = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyHash = _keyHash;
     }
 
     function enter() public payable {
@@ -58,5 +72,37 @@ contract Lottery is Ownable {
             )
         ) % players.length;
         */
+
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        lotteryState = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        emit RequestedRandomness(requestId);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator to return the random number
+     */
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lotteryState == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet"
+        );
+
+        require(_randomness > 0, "random number not found");
+
+        randomness = _randomness;
+        uint256 indexOfWinner = randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        recentWinner.transfer(address(this).balance);
+
+        // Reset
+        players = new address payable[](0);
+        lotteryState = LOTTERY_STATE.CLOSED;
     }
 }
